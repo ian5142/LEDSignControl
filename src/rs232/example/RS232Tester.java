1d5449c2b3701130b11533cb53ada3d3dd6788e7
ian5142 <ian@birdisland.net>
2016-10-04 13:15:25 -0300

    Added comments and Test Sections. Runs on Raspberry pi.

--- src/rs232/example/RS232Tester.java
+++ src/rs232/example/RS232Tester.java
@@ -9,6 +9,8 @@ package rs232.example;
 public class RS232Tester {
 
     /**
+     * Creates a SeriesTwo and CheckDB objects
+     * calls the various methods in both classes
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -18,16 +20,18 @@ public class RS232Tester {
 //        System.out.println(c + " is equal to " + b);]
 
 ////Test Code
-//        SeriesTwo test2 = new SeriesTwo();
-//        for (int i = 0; i < 30; i++) {
-//            test2.write();
-//        }
+        
 //        String line = test2.read();
 //        
 //        System.out.println(line);
 
         CheckDB test = new CheckDB();
-        System.out.println("The message is: " + test.selectMessage());
+        String message = test.selectMessage();
+        SeriesTwo test2 = new SeriesTwo();
+        for (int i = 0; i < 30; i++) {
+            test2.write(message);
+        }
+        System.out.println("The message is: " + message);
         System.out.println("Isupdated is: " + test.selectisUpdated());
 //        for (int i = 0; i < 75; i++) {
 //            boolean success = tester.testWrite();
