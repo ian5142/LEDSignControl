d081a9df41644fc7e642c834d4be20708573161d
ian5142 <ian@birdisland.net>
2017-11-02 17:09:45 -0300

    Change of statement

--- src/rs232/example/SeriesTwoTester.java
+++ src/rs232/example/SeriesTwoTester.java
@@ -12,6 +12,6 @@ package rs232.example;
 public class SeriesTwoTester {
     public static void main(String[] args) {
         SeriesTwo test2 = new SeriesTwo();
-        String calculateChksum = test2.calculateChksum("int val = -32768; String hex = Integer.toHexString(val); int parsedResult = (int) Long.parseLong(hex, 16); System.out.println(parsedResult); That's how you can do it. The reason why it doesn't work your way: Integer.parseInt takes a signed int, while toHexString produces an unsigned result.");
+        String calculateChksum = test2.calculateChksum("a==4");
     }
 }
