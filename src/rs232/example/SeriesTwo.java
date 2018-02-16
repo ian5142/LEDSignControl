f2539e1cf07346601f9dd38f6eb01fd43a6e7716
ian5142 <ian@birdisland.net>
2018-02-16 16:00:01 -0400

    Renamed message string.

--- src/rs232/example/SeriesTwo.java
+++ src/rs232/example/SeriesTwo.java
@@ -127,8 +127,8 @@ public class SeriesTwo {
         char scroll = (char) 0x19;
 //        String message = (char) 0x1 + "1" + (char) 0x2 + "This is a test." + (char) 0x4 + "1" + "8C" + CR;
 //        String message = header + address + startMes + body + 3 + endMes + numseq + chksum + CR;
-        String nonVmessage = header + address + startMes + body + endMes + numseq + chksum + CR;
-        boolean success = tester.testWrite(nonVmessage);
+        String message = header + address + startMes + body + endMes + numseq + chksum + CR;
+        boolean success = tester.testWrite(message);
         if (success) {
             boolean acknowledge = readAck();
             while(! acknowledge) {
