faabe95a24bf6053ad0479056c94721301648706
ian5142 <ian@birdisland.net>
2018-02-21 10:20:21 -0400

    changed scroll character, not tested yet.

--- src/rs232/example/RS232Tester.java
+++ src/rs232/example/RS232Tester.java
@@ -33,7 +33,7 @@ public class RS232Tester {
                 if (!test.selectisUpdated()) {
                     String message = test.selectMessage();
                     if (test.selectscrollON())  {
-                        message = (char) 0x19 + message;
+                        message = (char) 0x25 + message;
                     }
                     test2.write(message);
                     
