55d48abfb6d39305300b848f1d9da63cbe7894b9
ian5142 <ian@birdisland.net>
2018-02-22 15:09:00 -0400

    UnComment call to checkblink.

--- src/rs232/example/SeriesTwo.java
+++ src/rs232/example/SeriesTwo.java
@@ -135,7 +135,7 @@ public class SeriesTwo {
 
 //        String message = header + address + startMes + body + endMes + numseq + 
 //                chksum + newline + CR;
-//        body = checkBlink(body);
+        body = checkBlink(body);
         chksum = calculateChksum(body, scroll);
 //        String message = (char) 0x1 + "1" + (char) 0x2 + "This is a test." + (char) 0x4 + "1" + "8C" + CR;
 //        String message = header + address + startMes + body + 3 + endMes + numseq + chksum + CR;
