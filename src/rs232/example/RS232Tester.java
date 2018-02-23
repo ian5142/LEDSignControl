69a44d09849448f1b6424ef4a4b51c0e818861f0
ian5142 <ian@birdisland.net>
2018-02-23 14:07:06 -0400

    Removed for loop.

--- src/rs232/example/RS232Tester.java
+++ src/rs232/example/RS232Tester.java
@@ -1,17 +1,18 @@
 package rs232.example;
 
 //import jssc.SerialPortException;
-
 /**
- * Creates a SeriesTwo and CheckDB objects
- * calls the various methods in both classes
+ * Creates a SeriesTwo and CheckDB objects calls the various methods in both
+ * classes
+ *
  * @author Ian Van Schaick
  */
 public class RS232Tester {
 
     /**
-     * Creates a SeriesTwo and CheckDB objects
-     * calls the various methods in both classes
+     * Creates a SeriesTwo and CheckDB objects calls the various methods in both
+     * classes
+     *
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -21,35 +22,32 @@ public class RS232Tester {
 //        System.out.println(c + " is equal to " + b);]
 
 ////Test Code
-        
 //        String line = test2.read();
 //        
 //        System.out.println(line);
-                try {
-                    SeriesTwo test2 = new SeriesTwo();
+        try {
+            SeriesTwo test2 = new SeriesTwo();
 //                    test2.readAck();
-            for (int j = 0; j<10; j++) {
-                CheckDB test = new CheckDB();
-                if (!test.selectisUpdated()) {
-                    String message = test.selectMessage();
-                    boolean scroll = false;
-                    if (test.selectscrollON())  {
-                        scroll = true;
-                    }
-                    test2.write(message, scroll);
-                    
-                    System.out.println("The message is: " + message);
-                    System.out.println("Isupdated is: " + test.selectisUpdated());
-                    boolean acknowledge = test2.readAck();
-                    //System.out.println("Acknowledged: " + acknowledge);
-                    test.setisUpdated();
-                    System.out.println("Isupdated is: " + test.selectisUpdated());
+            CheckDB test = new CheckDB();
+            if (!test.selectisUpdated()) {
+                String message = test.selectMessage();
+                boolean scroll = false;
+                if (test.selectscrollON()) {
+                    scroll = true;
                 }
-                Thread.sleep(15 * 1000);
+                test2.write(message, scroll);
+
+                System.out.println("The message is: " + message);
+                System.out.println("Isupdated is: " + test.selectisUpdated());
+                boolean acknowledge = test2.readAck();
+                //System.out.println("Acknowledged: " + acknowledge);
+                test.setisUpdated();
+                System.out.println("Isupdated is: " + test.selectisUpdated());
             }
+            Thread.sleep(15 * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    
+
 }
