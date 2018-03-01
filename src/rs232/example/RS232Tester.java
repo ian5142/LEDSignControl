40e7c18362dbf922f2a5fc55f3ef40000421318b
ian5142 <ian@birdisland.net>
2018-03-01 11:20:33 -0400

    Added code to handle port not connected error. Not tested.

--- src/rs232/example/RS232Tester.java
+++ src/rs232/example/RS232Tester.java
@@ -27,25 +27,30 @@ public class RS232Tester {
 ////        System.out.println(line);
         try {
             SeriesTwo test2 = new SeriesTwo();
-////                    test2.readAck();
             CheckDB test = new CheckDB();
-            while (true) {
-                if (!test.selectisUpdated()) {
-                    String message = test.selectMessage();
-                    boolean scroll = false;
-                    if (test.selectscrollON()) {
-                        scroll = true;
+
+            boolean portConnected = test2.getConnected();
+            if (!portConnected) {
+                test.setPortError();
+            } else {
+                while (true) {
+                    if (!test.selectisUpdated()) {
+                        String message = test.selectMessage();
+                        boolean scroll = false;
+                        if (test.selectscrollON()) {
+                            scroll = true;
+                        }
+                        test2.write(message, scroll);
+                        boolean acknowledge = test2.readAck();
+                        System.out.println("The message is: " + message);
+                        System.out.println("Isupdated is: " + test.selectisUpdated());
+
+                        //System.out.println("Acknowledged: " + acknowledge);
+                        test.setisUpdated();
+                        System.out.println("Isupdated is: " + test.selectisUpdated());
                     }
-                    test2.write(message, scroll);
-                    boolean acknowledge = test2.readAck();
-                    System.out.println("The message is: " + message);
-                    System.out.println("Isupdated is: " + test.selectisUpdated());
-                    
-                    //System.out.println("Acknowledged: " + acknowledge);
-                    test.setisUpdated();
-                    System.out.println("Isupdated is: " + test.selectisUpdated());
+                    Thread.sleep(15 * 1000);
                 }
-                Thread.sleep(15 * 1000);
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
