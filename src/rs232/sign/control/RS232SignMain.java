8a82b4011f5b171397a5f73b0d415aa92ef7b3c8
ian5142 <ian@birdisland.net>
2018-11-23 13:43:04 -0400

    Various changes, working. Commented out print statements.

--- src/rs232/sign/control/RS232SignMain.java
+++ src/rs232/sign/control/RS232SignMain.java
@@ -30,63 +30,69 @@ public class RS232SignMain {
         Date lastDownloaded = new Date();
         SimpleDateFormat simpleFormatter = new SimpleDateFormat("mm");
         String lastDownloadedStr = simpleFormatter.format(lastDownloaded);
-        
+        System.out.println("Starting");
         try {
             SeriesTwo test2 = new SeriesTwo();
             Date hour = simpleFormatter.parse("00");
             Date halfhour = simpleFormatter.parse("45");
+            
             while (true) {
-                boolean portConnected = test2.getConnected();
                 Date current = new Date();
                 String currentStr = simpleFormatter.format(current);
+                int count = 0;
                 //(current.after(hour) && current.before(halfhour)) && 
 //                if (current.after(hour) && current.before(halfhour)) {
-                    System.out.println("Getting Forecast");
-                    weather.getDoc();
-                    forecast = weather.getForecast();
-                    lastUpdated = weather.getLastUpdated();
+//                System.out.println("Getting Forecast");
+                weather.getDoc();
+                forecast = weather.getForecast();
+                lastUpdated = weather.getLastUpdated();
 //                }
-                ArrayList<String> sent = new ArrayList<> ();
-                for (int i = 0 ; i < forecast.size() ; i ++) {
-                    test2.write(forecast.get(i) , true);
-                    int length = forecast.get(i).length();
-                    if (length <= 10) {
-                        Thread.sleep(2 * 1000);
+                ArrayList<String> sent = new ArrayList<>();
+                for (int i = 0; i < forecast.size(); i++) {
+                    boolean portConnected = test2.getConnected();
+                    if (forecast.get(i).equals("Night")) {
+                        count++;
                     }
-                    else if (length > 10 && length <=20){
-                        Thread.sleep(4 * 1000);
-                    }
-                    else if (length > 20 && length <=40){
-                        Thread.sleep(6 * 1000);
-                    }
-                    else if (length > 40 && length <=60){
-                        Thread.sleep(8 * 1000);
-                    }
-                    else if (length > 60){
-                        Thread.sleep(8 * 1000);
-                    }
-                    //+ forecast.get(i+1)
-                    boolean acknowledge = test2.readAck();
-                    
-//                    System.out.println("The message is: " + i);
-                    //System.out.println("Isupdated is: " + test.selectisUpdated());
+                    if (count == 0) {
+                        test2.write(forecast.get(i), true);
+                        int length = forecast.get(i).length();
+                        if (length <= 10) {
+                            Thread.sleep(2 * 1000);
+                        } else if (length > 10 && length <= 20) {
+                            Thread.sleep(4 * 1000);
+                        } else if (length > 20 && length <= 40) {
+                            Thread.sleep(6 * 1000);
+                        } else if (length > 40 && length <= 60) {
+                            Thread.sleep(8 * 1000);
+                        } else if (length > 60) {
+                            Thread.sleep(8 * 1000);
+                        }
+                        //+ forecast.get(i+1)
+                        boolean acknowledge = test2.readAck();
 
-                    //System.out.println("Acknowledged: " + acknowledge);
+//                    System.out.println("The message is: " + i);
+                        //System.out.println("Isupdated is: " + test.selectisUpdated());
+                        //System.out.println("Acknowledged: " + acknowledge);
 //                    if (acknowledge) {
 //                        test.setisUpdated();
 //                    }
-                    //System.out.println("Isupdated is: " + test.selectisUpdated());
-                    
-                    boolean acknowledge6 = false;
-                    do {
-                        test2.writeFillChars();
-                        acknowledge6 = test2.readAck();
-                    } while (acknowledge6 == false);
-                    Thread.sleep(250);
-                    acknowledge = false;
-                    if (i + 7 == forecast.size() ) {
+                        //System.out.println("Isupdated is: " + test.selectisUpdated());
+                        boolean acknowledge6 = false;
+                        do {
+                            test2.writeFillChars();
+                            acknowledge6 = test2.readAck();
+                        } while (acknowledge6 == false);
+                        Thread.sleep(250);
+                        acknowledge = false;
+
+                    }
+                    if (i + 7 == forecast.size()) {
+                        test2.write(lastUpdated, true);
+                        boolean acknowledge2 = test2.readAck();
+                        Thread.sleep(2 * 1000);
                         break;
                     }
+                    
                 }
                 
             }
